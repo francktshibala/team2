dd12c1f7bb827edc375d4b2aa52e1b5577591f97
francktshibala <franck1tshibala@gmail.com>
2025-05-09 20:17:24 -0600

    Individual Activity W01: Add item to cart broken

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,24 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // Get the existing cart, or create an empty array if none exists
+  let cart = getLocalStorage("so-cart");
+
+  // If cart doesn't exist or isn't an array, initialize it as an empty array
+  if (!cart || !Array.isArray(cart)) {
+    cart = [];
+  }
+
+  // Add the new product to the cart array
+  cart.push(product);
+
+  // Save the updated cart back to localStorage
+  setLocalStorage("so-cart", cart);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
