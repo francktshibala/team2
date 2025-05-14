21eaee33949c63b1f12301e07cf44a495f394c1c
AzureFirehawk <madison.thomas094@gmail.com>
2025-05-14 12:25:55 -0500

    add ProductDetails.mjs

--- src/js/product.js
+++ src/js/product.js
@@ -1,31 +1,20 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
+const productID = getParam("product");
 
-function addProductToCart(product) {
-  // Get the existing cart, or create an empty array if none exists
-  let cart = getLocalStorage("so-cart");
+const product = new ProductDetails(productID, dataSource);
+product.init();
 
-  // If cart doesn't exist or isn't an array, initialize it as an empty array
-  if (!cart || !Array.isArray(cart)) {
-    cart = [];
-  }
+// // add to cart button event handler
+// async function addToCartHandler(e) {
+//   const product = await dataSource.findProductById(e.target.dataset.id);
+//   addProductToCart(product);
+// }
 
-  // Add the new product to the cart array
-  cart.push(product);
-
-  // Save the updated cart back to localStorage
-  setLocalStorage("so-cart", cart);
-}
-
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+// // add listener to Add to Cart button
+// document
+//   .getElementById("addToCart")
+//   .addEventListener("click", addToCartHandler);
