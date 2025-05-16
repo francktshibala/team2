289086305022c3b709a4586c0685bf113c0ae83d
ify1237ibiam <ifegwuifeanyi2021@gmail.com>
2025-05-16 16:20:52 +0100

    Fixed duplicate items in cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,4 +1,4 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
@@ -8,21 +8,32 @@ function renderCartContents() {
 
 function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
-
+    <a href="#" class="cart-card__image">
+      <img src="${item.Image}" alt="${item.Name}" />
+    </a>
+    <a href="#">
+      <h2 class="card__name">${item.Name}</h2>
+    </a>
+    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+    <p class="cart-card__quantity">qty: ${item.quantity}</p>
+    <p class="cart-card__price">$${item.FinalPrice}</p>
+  </li>`;
   return newItem;
 }
 
+function addToCart(item) {
+  const cartItems = getLocalStorage("so-cart");
+  const existingItem = cartItems.find((cartItem) => cartItem.Id === item.Id);
+  if (existingItem) {
+    existingItem.quantity += 1;
+  } else {
+    item.quantity = 1;
+    cartItems.push(item);
+  }
+  setLocalStorage("so-cart", cartItems);
+  renderCartContents();
+}
+
 renderCartContents();
+
+export { addToCart };
