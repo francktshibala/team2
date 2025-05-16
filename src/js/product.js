8f914bdc5f37443278bd5b65b2f39e6abb4ed87f
francktshibala <149639827+francktshibala@users.noreply.github.com>
2025-05-16 12:41:04 -0600

    Revert "debug and fix adding to cart"

--- src/js/product.js
+++ src/js/product.js
@@ -1,4 +1,4 @@
-
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 import CartCount from "./CartCount.mjs";
@@ -6,6 +6,8 @@ import CartCount from "./CartCount.mjs";
 const dataSource = new ProductData("tents");
 const productID = getParam("product");
 
+const product = new ProductDetails(productID, dataSource);
+product.init();
 
 const cartCount = new CartCount(document.querySelector(".cart"));
 cartCount.render();
@@ -20,5 +22,3 @@ cartCount.render();
 // document
 //   .getElementById("addToCart")
 //   .addEventListener("click", addToCartHandler);
-
-
