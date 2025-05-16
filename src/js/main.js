0e731347869b7a5f5e8fda8f52c885f1c3270ec6
AzureFirehawk <madison.thomas094@gmail.com>
2025-05-16 10:28:22 -0500

    Converted Cart Count to a class, added to other JS files to populate on all pages

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
-import { cartCountTemplate, updateCartCount } from "./CartContents.mjs";
+import CartCount from "./CartCount.mjs";
 
 const dataSource = new ProductData("tents");
 const element = document.querySelector(".product-list");
@@ -8,8 +8,5 @@ const element = document.querySelector(".product-list");
 const productList = new ProductList("Tents", dataSource, element);
 productList.init();
 
-const cartIcon = document.querySelector(".cart")
-const cartCount = cartCountTemplate()
-
-cartIcon.insertAdjacentHTML("afterbegin", cartCount)
-updateCartCount()
+const cartCount = new CartCount(document.querySelector(".cart"));
+cartCount.render();
