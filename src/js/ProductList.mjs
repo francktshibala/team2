1d14c678c8c80f2425e255780cb698742d67dea7
AzureFirehawk <madison.thomas094@gmail.com>
2025-05-23 13:37:41 -0500

    Added sorting functionality to product listing pages

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -15,12 +15,13 @@ export default class ProductList {
     constructor(category, dataSource, listElement) {
         this.category = category;
         this.dataSource = dataSource;
-        this.listElement = listElement;
+      this.listElement = listElement;
+      this.products = [];
     }
 
     async init() {
-        const list = await this.dataSource.getData(this.category)
-        this.renderList(list);
+        this.products = await this.dataSource.getData(this.category)
+        this.renderList(this.products);
     }
 
     renderList(list) {
--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -2,6 +2,7 @@ import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 import CartCount from "./CartCount.mjs";
 import { loadHeaderFooter, getParam } from "./utils.mjs";
+import { sortProducts } from "./ProductSort.mjs";
 
 // 1. Call our function to load the header and footer
 loadHeaderFooter();
@@ -27,3 +28,34 @@ const heading = document.querySelector("h2");
 const categoryText = category.replace("-", " ").split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
 heading.textContent = "Top Products: " + categoryText;
 title.textContent = `Sleep Outside | ${categoryText}`;
+
+// set up sorting dropdown
+const sortOptions = [
+  {value: "default", text: ""},
+  {value: "name-a-z", text: "Name: A → Z"},
+  {value: "name-z-a", text: "Name: Z → A"},
+  {value: "price-low-to-high", text: "Price: Low to High"},
+  {value: "price-high-to-low", text: "Price: High to Low"}
+]
+
+const select = document.createElement("select");
+const sortLabel = document.createElement("label");
+sortLabel.setAttribute("for", "sortSelect");
+sortLabel.textContent = "Sort by: ";
+document.querySelector("#sortContainer").appendChild(sortLabel);
+select.id = "sortSelect";
+select.name = "sortSelect";
+
+sortOptions.forEach((option) => {
+    const optionElement = document.createElement("option");
+    optionElement.value = option.value;
+    optionElement.text = option.text;
+    select.add(optionElement);
+});
+document.querySelector("#sortContainer").appendChild(select);
+
+const sortSelect = document.getElementById("sortSelect");
+sortSelect.addEventListener("change", () => {
+    const sorted = sortProducts(productList.products, sortSelect.value);
+    productList.renderList(sorted);
+});
