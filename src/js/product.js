56f09e7c52a534851a130b70d874f5ebeeae3a8d
the-things-which-matter <igabrigham9@gmail.com>
2025-05-27 18:10:53 +0300

    w02 individual dynamic product list worked on

--- src/js/product.js
+++ src/js/product.js
@@ -2,6 +2,7 @@ import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 import CartCount from "./CartCount.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
 const productID = getParam("product");
@@ -13,6 +14,23 @@ const cartCount = new CartCount(document.querySelector(".cart"));
 cartCount.render();
 cartCount.listenForUpdates();
 
+function addProductToCart(product) {
+  let cart = getLocalStorage("so-cart") || [];
+
+  // Find if product already in cart
+  const existing = cart.find((item) => item.Id === product.Id);
+
+  if (existing) {
+    // If exists, increase quantity by 1 (or initialize quantity to 2)
+    existing.quantity = (existing.quantity || 1) + 1;
+  } else {
+    // Create a shallow copy of product and add quantity: 1
+    const productWithQuantity = { ...product, quantity: 1 };
+    cart.push(productWithQuantity);
+  }
+
+  setLocalStorage("so-cart", cart);
+}
 // // add to cart button event handler
 // async function addToCartHandler(e) {
 //   const product = await dataSource.findProductById(e.target.dataset.id);
